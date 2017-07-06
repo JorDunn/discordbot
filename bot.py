9113162537779de600629cf6b9c6df78caca36a6
Jordan Dunn <jordan@nodetwo.net>
2017-07-06 14:40:12 -0500

    Bot can now ping

--- bot.py
+++ bot.py
@@ -1,7 +1,24 @@
 import discord
 from discord.ext import commands, ipc
+import asyncio
 import random
+from config import Config
+
+conf = Config('bot.json')
 
 bot = commands.Bot(command_prefix='!', description="Random bot")
 
-bot.run()
+
+@bot.event
+async def on_ready():
+    print("Logged in as", bot.user.name, bot.user.id)
+
+@bot.event
+async def on_message(message):
+    if message.content.startswith('!ping'):
+        await asyncio.sleep(1)
+        msg = await bot.send_message(message.channel, "Pong")
+        await asyncio.sleep(2)
+        await bot.delete_message(msg)
+
+bot.run(conf.token)
