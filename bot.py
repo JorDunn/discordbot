575a303a3e6fbbae058b1e9cb7d77927f41a576e
Jordan Dunn <jordan@nodetwo.net>
2017-07-07 01:07:28 -0500

    It connects and says rude stuff currently

--- bot.py
+++ bot.py
@@ -5,6 +5,8 @@ import random
 from config import Config
 
 conf = Config('bot.json')
+print("Token: ", conf.token)
+print("Default channel: ", conf.default_channel)
 
 bot = commands.Bot(command_prefix='!', description="Random bot")
 
@@ -20,5 +22,8 @@ async def on_message(message):
         msg = await bot.send_message(message.channel, "Pong")
         await asyncio.sleep(2)
         await bot.delete_message(msg)
+    elif message.content.startswith('!rude'):
+        await asyncio.sleep(1)
+        msg = await bot.send_message(message.channel, "Your mother was a hamster, and your father smelt of elderberries!")
 
 bot.run(conf.token)
--- config.py
+++ config.py
@@ -1,6 +1,11 @@
+import json
+
 class Config(object):
     token           = None
     default_channel = None
 
     def __init__(self, filename):
-        pass
+        fp = open(filename)
+        contents = json.load(fp)
+        self.token = contents['token']
+        self.default_channel = contents['default_channel']
