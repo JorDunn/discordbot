8f682b39d4b66bb03f9f8faf4fe8119c7f9b9f52
Jordan Dunn <jordan@nodetwo.net>
2017-07-12 12:28:08 -0500

    Added typing() and send_temp_message() to avoid duplicate code

--- run.py
+++ run.py
@@ -11,26 +11,47 @@ conf = Config('bot.json')
 
 bot_commands = ['!help', '!ping']
 
+
+async def typing(channel):
+    await bot.send_typing(channel)
+    await asyncio.sleep(1)
+
+
+async def send_temp_message(channel, message, sleep):
+    await typing(channel)
+    msg = await bot.send_message(channel, message)
+    await asyncio.sleep(sleep)
+    await bot.delete_message(msg)
+
+
 @bot.event
 async def on_ready():
     print("Logged in as", bot.user.name, "(ID:", bot.user.id, ")")
     await bot.change_presence(game=discord.Game(name='Diablo 4'))
-    quotes = Quotes()
+
 
 @bot.event
 async def on_message(message):
-    if message.content.startswith('!help') and message.author != bot.user.id:
-        msg = await bot.send_message(message.channel, "Here are my current commands")
-        list_of_commands = ""
+    if message.content.startswith('!help') and message.author != bot.user:
+        await typing(message.channel)
+        await bot.send_message(message.channel, "Here are my current commands:")
+        command_list = ""
         for command in bot_commands:
-            list_of_commands += command + ", "
-        msg = await bot.send_message(message.channel, list_of_commands)
+            command_list += command + " "
+        await bot.send_message(message.channel, command_list)
     elif message.content.startswith('!ping'):
-        await asyncio.sleep(1)
-        msg = await bot.send_message(message.channel, "Pong")
-        await asyncio.sleep(2)
-        await bot.delete_message(msg)
-        await bot.delete_message(message)
+        if '!ping' in bot_commands:
+            await typing(message.channel)
+            await bot.send_message(message.channel, "Pong")
+        else:
+            await send_temp_message(message.channel, "This command has been disabled.", 5)
+    elif message.content.startswith('!quote'):
+        if '!quote' in bot_commands:
+            await typing(message.channel)
+            await bot.send_message(message.channel, "This command is not implemented yet.")
+        else:
+            await send_temp_message(message.channel, "This command has been disabled.", 5)
+
 
 if __name__ == '__main__':
     bot.run(conf.token)
